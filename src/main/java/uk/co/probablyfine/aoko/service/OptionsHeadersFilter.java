72add05ce3c212714b998915c621e011b501bbbf
Alex Wilson <a.wilson@alumni.warwick.ac.uk>
2013-04-29 22:45:16 +0100

    Allowed requests

--- src/main/java/uk/co/probablyfine/aoko/service/OptionsHeadersFilter.java
+++ src/main/java/uk/co/probablyfine/aoko/service/OptionsHeadersFilter.java
@@ -16,6 +16,7 @@ public class OptionsHeadersFilter implements Filter {
 		HttpServletResponse response = (HttpServletResponse) res;
 		response.setHeader("Access-Control-Allow-Origin", "*");
 		response.setHeader("Access-Control-Allow-Methods", "GET,POST");
+		response.setHeader("Access-Control-Allow-Headers", "X-Requested-With");
 		response.setHeader("Access-Control-Max-Age", "360");
 		chain.doFilter(req, res);
 	}
